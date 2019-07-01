752055a05bc3fe054d7b941083707b020ff34428
TillKok <Cty960904@163.com>
2019-07-01 22:18:01 +0800

    用户登录-页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,44 @@
 <template>
-  <div id="app">
-    <p>Login Component</p>
+  <div class="login-wrap">
+    <!-- 给组件加 class， 会把这个 class 作用到组建的根元素上 -->
+    <el-form class="login-from" ref="form" :model="form">
+      <el-form-item>
+        <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.code" placeholder="验证码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
   name: 'AppLogin',
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: '' // 后台数据名称
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.login-wrap {
+  height: 100%;
+  background-color: blueviolet;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
