8458b4c0965383f6b4c83e6c094d0044d8789b94
TillKok <Cty960904@163.com>
2019-07-02 22:13:05 +0800

    用户登录-表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,11 +5,15 @@
       <div class="form-head">
         <img src="./logo_index.png" alt="黑马头条号">
       </div>
-      <el-form class="form-content" ref="form" :model="form">
-        <el-form-item>
+      <el-form
+       class="form-content"
+       ref="form"
+       :model="form"
+       :rules="rules">
+        <el-form-item prop="mobile">
           <el-input v-model="form.mobile" placeholder="手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- el-col 栅格布局 -->
             <el-col :span="14">
               <el-input v-model="form.code" placeholder="验证码">验证码</el-input>
@@ -36,6 +40,16 @@ export default {
       form: {
         mobile: '',
         code: '' // 后台数据名称
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { len: 11, message: '长度必须为11位', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '长度必须为6位', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -43,6 +57,17 @@ export default {
     handleLogin () {
       // console.log('handleLogin!')
       // const { mobile, code } = this.form
+      // 使用 form 组件的validate 方法触发校验，获取校验的结果状态
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        // 表单验证通过，提交登录请求
+        this.submitLogin()
+      })
+    },
+
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -61,6 +86,7 @@ export default {
           this.$message.error('登录失败，手机号或验证码错误')
         ])
     },
+
     handleSendCode () {
       const { mobile } = this.form
       axios({
