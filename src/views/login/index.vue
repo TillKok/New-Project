ae253770d2477f3ec73a8e2976c1888621c86d2c
TillKok <Cty960904@163.com>
2019-07-02 21:42:29 +0800

    用户登录-发送验证码完成基本流程

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,7 +19,7 @@
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button type="primary" @click="handleLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -40,8 +40,26 @@ export default {
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    handleLogin () {
+      // console.log('handleLogin!')
+      // const { mobile, code } = this.form
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: this.form
+      }).then(res => { // >=200 && < 400的状态码会进入 then 成功
+        // console.log(res.data)
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        this.$router.push({
+          name: 'home'
+        })
+      })
+        .catch((e) => [
+          this.$message.error('登录失败，手机号或验证码错误')
+        ])
     },
     handleSendCode () {
       const { mobile } = this.form
