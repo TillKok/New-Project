44c202420c638fb301610a8b859f3f4a04b80c72
TillKok <Cty960904@163.com>
2019-07-02 20:49:03 +0800

    用户登录-发送验证码-使用极验初始化验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -28,6 +28,7 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt' // 引入极验 JavaScript SDK 文件， 通过 window.initGeetest 使用
+
 export default {
   name: 'AppLogin',
   data () {
@@ -48,7 +49,24 @@ export default {
         method: 'GET',
         url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
-        console.log(res.data)
+        const { data } = res.data
+        window.initGeetest({
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind' // 隐藏，直接弹出
+        }, function (captchaObj) {
+          captchaObj.onReady(function () {
+            // 验证ready之后才能调用verify方法显示验证码
+            captchaObj.verify() // 弹出验证码内容框
+          }).onSuccess(function () {
+            console.log(captchaObj.getValidate())
+          }).onError(function () {
+          })
+          // 在这里注册“发送验证码” 按钮的点击事件，然后验证用户是否用户是否输入手机号及手机格式是否正确
+          // captchaObj.verify
+        })
       })
     }
   }
